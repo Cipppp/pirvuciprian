65f01341d758dbe28724584373a5c5d643b17cb4
Cipppp <pirvuciprian37@gmail.com>
2022-03-03 12:20:02 +0200

    Updates

---
+++ precache-manifest.68bb39ca7f79ddddf5d00befc29795eb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "264f7faab4dbf25deb67ee5cce22b5cd",
+    "url": "/pirvuciprian/index.html"
+  },
+  {
+    "revision": "f2db54183e8ff51f2b65",
+    "url": "/pirvuciprian/static/css/2.b8803beb.chunk.css"
+  },
+  {
+    "revision": "4ff6b062fee653bc3a2e",
+    "url": "/pirvuciprian/static/css/main.7659c8c1.chunk.css"
+  },
+  {
+    "revision": "f2db54183e8ff51f2b65",
+    "url": "/pirvuciprian/static/js/2.58b89927.chunk.js"
+  },
+  {
+    "revision": "a69c5be555bd23c54e00",
+    "url": "/pirvuciprian/static/js/3.d144f80f.chunk.js"
+  },
+  {
+    "revision": "4ff6b062fee653bc3a2e",
+    "url": "/pirvuciprian/static/js/main.c7fb6cc5.chunk.js"
+  },
+  {
+    "revision": "770562d354348b34265b",
+    "url": "/pirvuciprian/static/js/runtime~main.24f7cebc.js"
+  },
+  {
+    "revision": "39bd102a108f160d868019252303e752",
+    "url": "/pirvuciprian/static/media/doodle.39bd102a.svg"
+  },
+  {
+    "revision": "f61b2ee173222e986cb07cadc4efc9ed",
+    "url": "/pirvuciprian/static/media/project1.f61b2ee1.png"
+  },
+  {
+    "revision": "f19e4fe6083cecbaf648ef389c7662e1",
+    "url": "/pirvuciprian/static/media/project2.f19e4fe6.png"
+  },
+  {
+    "revision": "05631a99abb05e0d5e1c4fa639e32cb5",
+    "url": "/pirvuciprian/static/media/project3.05631a99.png"
+  }
+]);
